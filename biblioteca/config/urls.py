4c9e79d14f6453430eb860d57a6df89fe30bab45
AbdalaErik <144078376+AbdalaErik@users.noreply.github.com>
2023-09-11 22:55:00 -0300

    Versão 3

--- biblioteca/config/urls.py
+++ biblioteca/config/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('consulta/', consulta, name='consultas'),
+    path('autores/', autores, name='autores'),
+    path('editoras/', editoras, name='editoras'),
+    path('categorias/', categorias, name='categorias'),
+    path('reserva/', reserva, name='reservas'),
 ]
